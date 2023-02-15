632003f500a9b2beb390889c26fc8055f4169007
SirisakUnknowss <82793984+SirisakUnknowss@users.noreply.github.com>
2023-02-15 22:38:05 +0700

    update add number

--- web/static/assets/js/add/checkInput.js
+++ web/static/assets/js/add/checkInput.js
@@ -1,50 +1,17 @@
 
-const input_length1 = document.querySelector("#input_length1")
-const input_length2 = document.querySelector("#input_length2")
-const input_length3 = document.querySelector("#input_length3")
-const input_length4 = document.querySelector("#input_length4")
-const input_length5 = document.querySelector("#input_length5")
-const input_length6 = document.querySelector("#input_length6")
+const input_length = document.querySelector("#input_length")
 const confirmAddNumber = document.querySelector("#confirmAddNumber")
 const formSendNumber = document.querySelector("#formSendNumber")
 const addOneNumber = document.querySelector("#addOneNumber")
 
-function inputLength1()
+function inputLength()
 {
-    if (input_length1.value.length > 0) input_length2.focus()
-    else input_length1.focus()
-}
-function inputLength2()
-{
-    if (input_length2.value.length > 0) input_length3.focus()
-    else input_length1.focus()
-}
-function inputLength3()
-{
-    if (input_length3.value.length > 0) input_length4.focus()
-    else input_length2.focus()
-}
-function inputLength4()
-{
-    if (input_length4.value.length > 0) input_length5.focus()
-    else input_length3.focus()
-}
-function inputLength5()
-{
-    if (input_length5.value.length > 0) input_length6.focus()
-    else input_length4.focus()
-}
-function inputLength6()
-{
-    if (input_length6.value.length > 0) input_length6.focus()
-    else input_length5.focus()
+    if (input_length.value.length < 6) input_length.focus()
 }
 
 confirmAddNumber.addEventListener('click', event => {
     shopSelect.value = shopSelect.options[shopSelect.selectedIndex].value
-    if (input_length1.value.length == 0 || input_length2.value.length == 0 ||
-        input_length3.value.length == 0 || input_length4.value.length == 0 ||
-        input_length5.value.length == 0 || input_length6.value.length == 0) return alert("กรุณากรอกหมายเลขให้ครบถ้วน!")
+    if (input_length.value.length != 6) return alert("กรุณากรอกหมายเลขให้ครบถ้วน!")
     else { checkTypeNumber() }
     event.preventDefault()
     
@@ -52,39 +19,14 @@ confirmAddNumber.addEventListener('click', event => {
 
 function checkTypeNumber()
 {
-    if (isNaN(input_length1.value))
+    if (isNaN(input_length.value))
     {
-        input_length1.focus()
-        return alert("กรุณากรอกหมายเลขให้ถูกต้อง!")
-    }
-    else if (isNaN(input_length2.value))
-    {
-        input_length2.focus()
-        return alert("กรุณากรอกหมายเลขให้ถูกต้อง!")
-    }
-    else if (isNaN(input_length3.value))
-    {
-        input_length3.focus()
-        return alert("กรุณากรอกหมายเลขให้ถูกต้อง!")
-    }
-    else if (isNaN(input_length4.value))
-    {
-        input_length4.focus()
-        return alert("กรุณากรอกหมายเลขให้ถูกต้อง!")
-    }
-    else if (isNaN(input_length5.value))
-    {
-        input_length5.focus()
-        return alert("กรุณากรอกหมายเลขให้ถูกต้อง!")
-    }
-    else if (isNaN(input_length6.value))
-    {
-        input_length6.focus()
+        input_length.focus()
         return alert("กรุณากรอกหมายเลขให้ถูกต้อง!")
     }
     else
     {
-        addOneNumber.value = (input_length1.value + input_length2.value + input_length3.value + input_length4.value + input_length5.value + input_length6.value)
+        addOneNumber.value = (input_length.value)
         formSendNumber.submit()
     }
 }
--- web/static/assets/js/addMany/checkInput.js
+++ web/static/assets/js/addMany/checkInput.js
@@ -1,10 +1,5 @@
 
-const input_length1 = document.querySelector("#input_length1")
-const input_length2 = document.querySelector("#input_length2")
-const input_length3 = document.querySelector("#input_length3")
-const input_length4 = document.querySelector("#input_length4")
-const input_length5 = document.querySelector("#input_length5")
-const input_length6 = document.querySelector("#input_length6")
+const input_length = document.querySelector("#input_length")
 const confirmAddManyNumber = document.querySelector("#confirmAddManyNumber")
 const formSendNumber = document.querySelector("#formSendNumber")
 const formSendManyNumber = document.querySelector("#formSendManyNumber")
@@ -12,35 +7,9 @@ const addOneNumber = document.querySelector("#addOneNumber")
 // const shopAddSelect = document.querySelector("#shopSelect")
 // const shopAddManySelect = document.querySelector("#shopAddManySelect")
 
-function inputLength1()
+function inputLength()
 {
-    if (input_length1.value.length > 0) input_length2.focus()
-    else input_length1.focus()
-}
-function inputLength2()
-{
-    if (input_length2.value.length > 0) input_length3.focus()
-    else input_length1.focus()
-}
-function inputLength3()
-{
-    if (input_length3.value.length > 0) input_length4.focus()
-    else input_length2.focus()
-}
-function inputLength4()
-{
-    if (input_length4.value.length > 0) input_length5.focus()
-    else input_length3.focus()
-}
-function inputLength5()
-{
-    if (input_length5.value.length > 0) input_length6.focus()
-    else input_length4.focus()
-}
-function inputLength6()
-{
-    if (input_length6.value.length > 0) input_length6.focus()
-    else input_length5.focus()
+    if (input_length.value.length < 6) input_length.focus()
 }
 
 confirmAddManyNumber.addEventListener('click', event => {
@@ -52,39 +21,14 @@ confirmAddManyNumber.addEventListener('click', event => {
 
 function checkTypeNumber()
 {
-    if (isNaN(input_length1.value))
+    if (isNaN(input_length.value))
     {
-        input_length1.focus()
-        return alert("กรุณากรอกหมายเลขให้ถูกต้อง!")
-    }
-    else if (isNaN(input_length2.value))
-    {
-        input_length2.focus()
-        return alert("กรุณากรอกหมายเลขให้ถูกต้อง!")
-    }
-    else if (isNaN(input_length3.value))
-    {
-        input_length3.focus()
-        return alert("กรุณากรอกหมายเลขให้ถูกต้อง!")
-    }
-    else if (isNaN(input_length4.value))
-    {
-        input_length4.focus()
-        return alert("กรุณากรอกหมายเลขให้ถูกต้อง!")
-    }
-    else if (isNaN(input_length5.value))
-    {
-        input_length5.focus()
-        return alert("กรุณากรอกหมายเลขให้ถูกต้อง!")
-    }
-    else if (isNaN(input_length6.value))
-    {
-        input_length6.focus()
+        input_length.focus()
         return alert("กรุณากรอกหมายเลขให้ถูกต้อง!")
     }
     else
     {
-        addOneNumber.value = (input_length1.value + input_length2.value + input_length3.value + input_length4.value + input_length5.value + input_length6.value)
+        addOneNumber.value = (input_length.value)
         formSendNumber.submit()
     }
 }
