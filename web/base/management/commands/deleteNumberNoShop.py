3abd6c3bf1692545eaa9a94b694388f404c54ed6
Yaijino <sirisak.unknowss@gmail.com>
2024-09-05 07:12:41 +0700

    Update deleteNumberNoShop.py

--- web/base/management/commands/deleteNumberNoShop.py
+++ web/base/management/commands/deleteNumberNoShop.py
@@ -15,7 +15,14 @@ class Command(BaseCommand):
     
     def removeDataAll(self):
         numberList = NumberLottery.objects.all()
+        count = 0
         for number in numberList:
             if not Shop.objects.filter(pk=number.idShop).exists():
-                print(f"number === {number.numberLottery}")
+                # print(f"number === {number.numberLottery}")
+                count += 1
+                
+        print(numberList.count())
+        print(count)
+
+        self.stdout.write(self.style.SUCCESS('Remove All Complete'))
         
